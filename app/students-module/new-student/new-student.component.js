6effb0a35e92bc056ebc2aa1b605ab7922524bdc
Pham Van Lam <phamvanlam1991@gmail.com>
2019-12-02 08:52:54 +0700

    add create student form

--- app/students-module/new-student/new-student.component.js
+++ app/students-module/new-student/new-student.component.js
@@ -2,9 +2,12 @@ angular.
     module('students').
     component('newStudent', {
         templateUrl:'students-module/new-student/new-student.template.html',
-        controller: [function NewStudentController(){
-            createStudent = function(){
+        controller: function NewStudentController(){
+            this.create = function(){
                 console.log(this.newStudent);
+            };
+            this.reset = function(){
+                this.newStudent = {};
             }
-        }]
-    })
+        }
+    });
