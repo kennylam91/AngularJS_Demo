c3f98e8032fc5d39aeea0492bc23cc55c9b4839a
Pham Van Lam <phamvanlam1991@gmail.com>
2019-12-02 09:50:39 +0700

    add StudentService to student-detail

--- app/students-module/student-detail/student-detail.component.js
+++ app/students-module/student-detail/student-detail.component.js
@@ -2,13 +2,12 @@ angular.
     module('students').
     component('studentDetail', {
         templateUrl: 'students-module/student-detail/student-detail.template.html',
-        controller: ['$routeParams', '$http',
-            function StudentDetailController($routeParams, $http) {
+        controller: ['$routeParams', 'StudentService',
+            function StudentDetailController($routeParams, StudentService) {
                 let self = this;
-                console.log($routeParams.studentCode);
-                $http.get('students-module/students/' + $routeParams.studentCode + '.json')
-                    .then(function (response) {
-                        self.student = response.data;
-                    })
+                StudentService.getStudentByCode($routeParams.studentCode).then(function(response){
+                    self.student = response.data;
+                })
+            
             }]
     });
--- app/students-module/students.service.js
+++ app/students-module/students.service.js
@@ -1,6 +1,9 @@
 angular.module('students')
-    .service('StudentService',['$http' ,function ($http) {
+    .service('StudentService', ['$http', function ($http) {
         this.getStudents = function () {
             return $http.get('students-module/students/students.json');
+        }
+        this.getStudentByCode = function (studentCode) {
+            return $http.get('students-module/students/' + studentCode + '.json');
         }
     }])
